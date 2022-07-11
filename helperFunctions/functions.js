1ddf8195ddce519ea0cf1630f3d6e0e46c56e6d6
Larry Simiyu <larry.simiyu@gmail.com>
2022-07-11 03:36:25 -0700

    first commit

---
+++ helperFunctions/functions.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const axios = require("axios");
+
+const getLocalLonLat = () => {
+  return axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=95821&key=${process.env.GOOGLE_API}`
+    )
+    .then(({ data }) => {
+      const { location } = data.results[0].geometry;
+      return location;
+    });
+};
+const getWeatherData = (lat, long) => {
+  return axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${process.env.WEATHER_KEY}`
+    )
+    .then(({ data }) => {
+      return data;
+    });
+};
+
+const getZipLonLat = (zipCode) => {
+  return axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${zipCode}&key=${process.env.GOOGLE_API}`
+    )
+    .then(({ data }) => {
+      const { location } = data.results[0].geometry;
+      return location;
+    });
+};
+
+module.exports = { getLocalLonLat, getWeatherData, getZipLonLat };
